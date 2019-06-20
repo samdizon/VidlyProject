556d23c589746644c258804f1286c8c921905df2
Sophia Anne Marie Dizon <spangidian@contractor.temenos.com>
2019-06-20 14:02:39 +0800

    added custom 18 years old validation if not PayAsYouGo Membership Type, adde display of validation error summary

--- Vidly/Models/Customer.cs
+++ Vidly/Models/Customer.cs
@@ -16,6 +16,7 @@ namespace Vidly.Models
         public string Name { get; set; }
 
         [Display(Name = "Date of Birth")]
+        [Min18YearsIfAMember]
         public DateTime? Birthdate { get; set; }
 
         public bool IsSubscribedToNewsletter { get; set; }
