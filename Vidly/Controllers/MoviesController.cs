ace27609db88c46e324eeecd9b0b92ab315ca701
Sophia Anne Marie Dizon <spangidian@contractor.temenos.com>
2019-06-24 12:29:26 +0800

    added authorize filter on home, movies, and customer controllers

--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -9,6 +9,7 @@ using Vidly.ViewModel;
 
 namespace Vidly.Controllers
 {
+    [Authorize]
     public class MoviesController : Controller
     {
         private readonly ApplicationDbContext _context;
