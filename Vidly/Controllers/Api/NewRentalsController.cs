d4edb8eca57f134d09470b48751526b9a8f45c70
Sophia Anne Marie Dizon <spangidian@contractor.temenos.com>
2019-06-28 09:37:22 +0800

    remove validation checks for customer and movies on NewRental due to extra noise

--- Vidly/Controllers/Api/NewRentalsController.cs
+++ Vidly/Controllers/Api/NewRentalsController.cs
@@ -19,19 +19,11 @@ namespace Vidly.Controllers.Api
         }
 
         [HttpPost]
-        public IHttpActionResult CreateNewRentals(NewRentalDto newRental) {
-
-            if (newRental.MovieIds.Count == 0)
-                return BadRequest("No movieIds have been given.");
+        public IHttpActionResult CreateNewRentals(NewRentalDto newRental)
+        {
             var customer = _context.Customers.SingleOrDefault(c => c.Id == newRental.CustomerId);
 
-            if (customer == null)
-                return BadRequest("CustomerId is invalid.");
-
             var movies = _context.Movies.Where(m => newRental.MovieIds.Contains(m.Id)).ToList();
-
-            if (movies.Count != newRental.MovieIds.Count)
-                return BadRequest("One or more MovieIds are invalid");
 
             foreach (var movie in movies)
             {
